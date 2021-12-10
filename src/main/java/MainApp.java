9cd0bd06537e7ae2316852777f33d08cb9a260a6
Saifferela <Saifferela@gmail.com>
2021-12-10 15:50:11 +0200

    Numbers added to the list.

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class MainApp {
     public static void main(String[] args){
-        List<Integer> numbers = List.of();
+        List<Integer> numbers = List.of(64,85,644,43,1,0,-100,-41,543,0, 4321,-5555, 12312);
 
         NumberFilter numberFilter = new NumberFilter(numbers);
 
